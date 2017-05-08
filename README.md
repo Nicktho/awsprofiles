7459cd773662ae860746f558702635a69a2715a2
Nick Matenaar <matenaar92@gmail.com>
2017-05-08 13:56:22 +1000

    Init

---
+++ README.md
@@ -0,0 +1,89 @@
+# awsprofiles
+
+> Manage and switch between AWS profiles
+
+<p align="center">
+  <img src="demo.gif" widthi="924" />
+</p>
+
+## What's this?
+
+* [CLI](#cli) to switch between saved AWS profiles
+* Alternative [Interactive UI](#interactive-ui)
+* Inspired by [np](https://github.com/sindresorhus/np)
+
+## Install
+
+```bash
+$ yarn add global awsprofiles
+```
+
+## CLI
+```bash
+$ awsprofiles --help
+
+  A tool to manage and switch between aws profiles
+
+  Usage
+    $ awsprofiles <profile>
+
+  Examples
+    $ awsprofiles
+    $ awsprofiles prod
+    $ awsprofiles dev
+```
+
+## Interactive UI
+Running `awsprofiles` without arguments provides an interactive UI similar to that of [np](https://github.com/sindresorhus/np)
+
+<p align="center">
+  <img src="screenshot.png" widthi="924" />
+</p>
+
+## Configuration
+
+Expects an `~/.aws/configure` file similar to the format below:
+
+```
+[default]
+aws_access_key_id = 1xxxxxxxxxxxxxxxxxxx
+aws_secret_access_key = 1xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+[production]
+aws_access_key_id = 1xxxxxxxxxxxxxxxxxxx
+aws_secret_access_key = 1xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+[sandbox]
+aws_access_key_id = 2xxxxxxxxxxxxxxxxxxx
+aws_secret_access_key = 2xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+[a_client]
+aws_access_key_id = 3xxxxxxxxxxxxxxxxxxx
+aws_secret_access_key = 3xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+[personal]
+aws_access_key_id = 4xxxxxxxxxxxxxxxxxxx
+aws_secret_access_key = 4xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
+
+## Tips
+
+### Alias
+
+I like to alias `awsprofiles` to `ap` by adding the following to `.zshrc` or `.bashrc`
+
+```
+alias ap = "awsprofiles"
+```
+
+## Special Thanks
+
+* [np](https://github.com/sindresorhus/np)
+
+## Created By
+
+* [Nick Matenaar](https://github.com/nicktho)
+
+## License
+
+[MIT (c) Nick Matenaar](LICENSE)
---
+++ cli.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const meow = require('meow');
+const updateNotifier = require('update-notifier');
+const chalk = require('chalk');
+const { prompt } = require('inquirer');
+
+const parseCredentials = require('./lib/parseCredentials');
+const generateCredentials = require('./lib/generateCredentials');
+
+const cli = meow(`
+  Usage
+    $ awsprofiles <profile>
+
+  Options
+    --credentials, -c Path to your credentials file (default: ~/.aws/credentials)
+
+  Examples
+    $ awsprofiles
+    $ awsprofiles prod
+    $ awsprofiles dev
+`, {
+  alias: { c: 'credentials' },
+  default: { credentials: path.join(os.homedir(), '.aws', 'credentials') },
+});
+
+updateNotifier({ pkg: cli.pkg }).notify();
+
+const credentialsFile = cli.flags.credentials;
+
+Promise.resolve()
+  .then(() => {
+    const credentials = parseCredentials(fs.readFileSync(credentialsFile, 'utf8'));
+    
+    if (!credentials.default) {
+      throw new Error('No default profile is set');
+    }
+
+    const currentDefaultProfile = Object.keys(credentials).find(profile => profile !== 'default' && credentials[profile].awsAccessKeyId === credentials.default.awsAccessKeyId);
+
+    if (!currentDefaultProfile) {
+      throw new Error('Current default profile is not named in profile list, please check README.md for valid configuration');
+    }
+
+    const profiles = Object.keys(credentials).filter(profile => profile !== 'default' && profile !== currentDefaultProfile);
+
+    if (!profiles) {
+      throw new Error(`Only profile ${chalk.magenta(currentDefaultProfile)} found and is set as default.`);
+    }
+
+    if (cli.input.length > 0) {
+      const input = cli.input[0];
+
+      if (input !== currentDefaultProfile && !profiles.includes(input)) {
+        throw new Error(`Profile ${chalk.magenta(input)} does not exist in ${chalk.magenta(credentialsFile)}`);
+      }
+
+      return {
+        profile: input,
+        credentials,
+      };
+    }
+
+    console.log('');
+    console.log(`Current AWS profile ${chalk.magenta(currentDefaultProfile)}`);
+    console.log('');
+
+    const prompts = [
+      {
+        type: 'list',
+        name: 'profile',
+        message: 'Select AWS profile',
+        choices: profiles,
+        validate: input => profiles.includes(input) || `Profile ${input} does not exist in ${chalk.magenta(credentialsFile)}`,
+      },
+    ];
+
+    return prompt(prompts).then(({ profile }) => ({ profile, credentials }));
+  })
+  .then(({ profile, credentials }) => {
+    const newCredentials = Object.assign({}, credentials, {
+      default: credentials[profile],
+    });
+
+    fs.writeFileSync(credentialsFile, generateCredentials(newCredentials), 'utf8');
+
+    return profile;
+  })
+  .then(profile => {
+    console.log('');
+    console.log(`🔑  Changed default profile to ${chalk.magenta(profile)} in ${chalk.magenta(credentialsFile)}`);
+  })
+  .catch(err => {
+    console.log('');
+    console.log(`❌  ${err}`);
+  });
+
